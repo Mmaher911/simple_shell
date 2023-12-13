c1f5f02b4bd6cbe1da2f0cab79c902ef229533e2
amintce <akingbadeadeyemi5566@gmail.com>
2023-12-13 14:49:56 -0800

    latenight

---
+++ builtin2.c
@@ -0,0 +1,87 @@
+#include "main.h"
+/**
+ * dis_env - Display the enviroment variable
+ * @cmd: parsed command check
+ * @st: last command executed status
+ * Return: Always 0
+ */
+int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int st)
+{
+	size_t i;
+	int len;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		len = _strlen(environ[i]);
+		write(1, environ[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	return (0);
+}
+/**
+ * echo_bul - execute the echo cases
+ * @st: last command executed status
+ * @cmd: parsed command
+ * Return: Always 1 Or execute normal echo
+ */
+int echo_bul(char **cmd, int st)
+{
+	char *path;
+	unsigned int pid = getppid();
+
+	if (_strncmp(cmd[1], "$?", 2) == 0)
+	{
+		print_number_int(st);
+		PRINT("\n");
+	}
+	else if (_strncmp(cmd[1], "$$", 2) == 0)
+	{
+		print_number(pid);
+		PRINT("\n");
+	}
+	else if (_strncmp(cmd[1], "$PATH", 5) == 0)
+	{
+		path = _getenv("PATH");
+		PRINT(path);
+		PRINT("\n");
+		free(path);
+	}
+	else
+		return (print_echo(cmd));
+
+	return (1);
+}
+/**
+ * history_dis - history of user display
+ * @c: command
+ * @st: last command executed status
+ * Return: 0 success or -1 if fail
+ */
+int history_dis(__attribute__((unused))char **c, __attribute__((unused))int st)
+{
+	char *filename = ".simple_shell_history";
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	int counter = 0;
+	char *er;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+	while ((getline(&line, &len, fp)) != -1)
+	{
+		counter++;
+		er = _itoa(counter);
+		PRINT(er);
+		free(er);
+		PRINT(" ");
+		PRINT(line);
+	}
+	if (line)
+		free(line);
+	fclose(fp);
+	return (0);
+}
